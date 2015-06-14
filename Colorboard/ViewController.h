22961a5893366df6e6879ab3fc563ee31898162d
wuqifu <wuqifu@live.cn>
2015-06-14 17:18:58 +0800

    Initial Commit

---
+++ Colorboard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Colorboard
+//
+//  Created by Wuqifu on 15/6/14.
+//  Copyright (c) 2015年 Wuqifu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Colorboard/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Colorboard
+//
+//  Created by Wuqifu on 15/6/14.
+//  Copyright (c) 2015年 Wuqifu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
